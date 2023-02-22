3f5a340bfcf97746ce45ef326e4f11ae534e31f8
Divyjoshi <divyjoshi1510@gmail.com>
2023-02-22 11:42:21 +0530

    Updated

---
+++ Lab 4/lab4.java
@@ -0,0 +1,88 @@
+import java.util.*;
+
+public class lab4 {
+    public static int accountNumber;
+    public static String accountHolderName;
+    public static double accountBalance;
+
+    public static void main(String[] args) {
+        ArrayList<String> transactions = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        initializeCustomer(scanner);
+        int choice;
+        do {
+            System.out.println("1. Deposit money");
+            System.out.println("2. Withdraw money");
+            System.out.println("3. Print transactions");
+            System.out.println("4. Print account summary");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    depositMoney(scanner, transactions);
+                    break;
+                case 2:
+                    withdrawMoney(scanner, transactions);
+                    break;
+                case 3:
+                    printTransactions(transactions);
+                    break;
+                case 4:
+                    printAccountSummary();
+                    break;
+                case 0:
+                    System.out.println("Goodbye"!);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != 0);
+        scanner.close();
+    }
+
+    public static void initializeCustomer(Scanner scanner) {
+        System.out.print("Enter account number: ");
+        accountNumber = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Enter account holder name: ");
+        accountHolderName = scanner.nextLine();
+        System.out.print("Enter account balance: ");
+        accountBalance = scanner.nextDouble();
+    }
+
+    public static void depositMoney(Scanner scanner, ArrayList<String> transactions) {
+        System.out.print("Enter deposit amount: ");
+        double amount = scanner.nextDouble();
+        accountBalance += amount;
+        String transaction = "Deposit of " + amount + " on " + java.time.LocalDate.now() + " at " + java.time.LocalTime.now();
+        transactions.add(transaction);
+    }
+
+    public static void withdrawMoney(Scanner scanner, ArrayList<String> transactions) {
+        System.out.print("Enter withdrawal amount: ");
+        double amount = scanner.nextDouble();
+        if (amount > accountBalance) {
+            System.out.println("Insufficient funds");
+        } else {
+            accountBalance -= amount;
+            String transaction = "Withdrawal of " + amount + " on " + java.time.LocalDate.now() + " at " + java.time.LocalTime.now();
+            transactions.add(transaction);
+        }
+    }
+
+    public static void printTransactions(ArrayList<String> transactions) {
+        System.out.println("Transaction history:");
+        for (String transaction : transactions) {
+            System.out.println(transaction);
+        }
+    }
+
+    public static void printAccountSummary() {
+        System.out.println("Account summary:");
+        System.out.println("Account number: " + accountNumber);
+        System.out.println("Account holder name: " + accountHolderName);
+        System.out.println("Account balance: " + accountBalance);
+    }
+}
---
+++ Lab 4/readme.md
@@ -0,0 +1,69 @@
+
+## Queston :- 
+
+### - Write a JAVA Menu driven program that does the following:
+
+#### You can ONLY have the below variables as global variables
+##### 1. Account Number
+##### 2. Account Holder Name
+##### 3. Account Balance
+
+#### You MUST have the below as functions
+##### 1. To initialize the customer
+##### 2. To deposit money
+##### 3. To withdraw money
+##### 4. To print the transactions
+##### 5. To print account summary
+
+#### Your menu will have the following operations once the customer is created
+##### 1. To deposit money
+##### 2. To withdraw money
+##### 3. To print the transactions
+##### 4. To print account summary
+
+
+## Output :- 
+
+Enter account number: 2564845
+Enter account holder name: Divy
+Enter account balance: 50000
+1. Deposit money
+2. Withdraw money
+3. Print transactions
+4. Print account summary
+0. Exit
+Enter your choice: 1
+Enter deposit amount: 200
+1. Deposit money
+2. Withdraw money
+3. Print transactions
+4. Print account summary
+0. Exit
+Enter your choice: 2
+Enter withdrawal amount: 6000
+1. Deposit money
+2. Withdraw money
+3. Print transactions
+4. Print account summary
+0. Exit
+Enter your choice: 3
+Transaction history:
+Deposit of 200.0 on 2023-02-22 at 11:38:34.834923800
+Withdrawal of 6000.0 on 2023-02-22 at 11:38:39.964921600
+1. Deposit money
+2. Withdraw money
+3. Print transactions
+4. Print account summary
+0. Exit
+Enter your choice: 4
+Account summary:
+Account number: 2564845
+Account holder name: Divy
+Account balance: 44200.0
+1. Deposit money
+2. Withdraw money
+3. Print transactions
+4. Print account summary
+0. Exit
+Enter your choice: 0
+Goodbye!
